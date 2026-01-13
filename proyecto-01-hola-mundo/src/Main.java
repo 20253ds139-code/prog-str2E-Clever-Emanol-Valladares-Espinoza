4dc95de101bcfb65f606416b39f0b80b32377e45
clever emanol valladares espinoza <20253ds139@utez.edu.mx>
2026-01-13 07:29:33 -0600

    se edita el archivo readme

--- proyecto-01-hola-mundo/src/Main.java
+++ proyecto-01-hola-mundo/src/Main.java
@@ -1,3 +1,8 @@
-void main() {
-    
+public class Main {
+    public static void main (String[]args){
+        System.out.println("Hola Mundo");
+        System.out.println("Clever Emanol Valladares Espinoza");
+        System.out.println("Segundo cuatrimestre Grupo:E");
+        System.out.println("09/1/26");
+    }
 }
