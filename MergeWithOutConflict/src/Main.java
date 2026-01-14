3c6f9b41fc4bc9707fa6b56e9a7044c7827b6726
clever emanol valladares espinoza <20253ds139@utez.edu.mx>
2026-01-13 20:21:19 -0600

    chore: base actividad git merge withoutconflict

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-    public static void main(String[] args){
-
-        System.out.println("Semana 2");
+    public static void main(String[]args){
+        System.out.println("semana 2");
         System.out.println("version base");
     }
 }
