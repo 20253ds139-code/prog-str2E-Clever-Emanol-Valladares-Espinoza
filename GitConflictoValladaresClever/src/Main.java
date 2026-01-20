ec397a32bbe91ce1677bc8e86568a2a1a603838f
clever emanol valladares espinoza <20253ds139@utez.edu.mx>
2026-01-19 22:43:29 -0600

    feat: agregar descuento por umbral

--- GitConflictoValladaresClever/src/Main.java
+++ GitConflictoValladaresClever/src/Main.java
@@ -6,9 +6,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
-        double IVA = 0.16;
-        double total = subtotal + (subtotal * IVA);
+        double DESCUENTO = 0.10;
+        double UMBRAL_DESCUENTO = 1000.0;
 
+        double total = subtotal;
+        if (subtotal > UMBRAL_DESCUENTO) {
+            total = total - (total * DESCUENTO);
+        }
 
         System.out.printf("Total a pagar: %.2f%n", total);
     }
