e741ebf5dd574f5a109243c1e7508e648ddab90a
clever emanol valladares espinoza <20253ds139@utez.edu.mx>
2026-02-04 21:46:47 -0600

    envio con clase

--- CotizadorClase/src/App.java
+++ CotizadorClase/src/App.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class App {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ShippingCalculator calc = new ShippingCalculator();
         double peso = Input.leerDoubleEnRango(sc,
